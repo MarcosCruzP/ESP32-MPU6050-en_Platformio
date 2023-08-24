e5b8d8aa847236d124cbb02bf5689873c81b2768
Marcos Cruz <marcos.cpascacio@gmail.com>
2023-08-24 17:18:24 -0600

    giros-acel

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,8 @@ int16_t acelX = 0, acelY = 0, acelZ=0;
 int16_t gyroscopeX = 0, gyroscopeY = 0, gyroscopeZ=0;
 float last_gyroscopeX = 0, last_gyroscopeY = 0, last_gyroscopeZ=0;
 
-unsigned long lastTime =0, simpleTime = 500, dt= 0;
+unsigned long lastTime =0, simpleTime = 100;
+long preview_time, dt;
 
 void setup() {
   Serial.begin(115200);
@@ -25,7 +26,7 @@ void loop() {
   if(millis()-lastTime>=simpleTime){
       
       lastTime = millis();
-      dt = millis()-lastTime;
+     
   
       // read aceleration
       mpu.getAcceleration(&acelX, &acelY, &acelZ);
@@ -33,31 +34,42 @@ void loop() {
       // Calculate angle of inclination 
       float aceel_anng_X = atan(acelX/sqrt(pow(acelY,2) + pow(acelZ,2))) *(180.0/3.14);
       float aceel_anng_Y = atan(acelY/sqrt(pow(acelX,2) + pow(acelZ,2))) *(180.0/3.14);
+      float aceel_anng_Z = atan(acelZ/sqrt(pow(acelX,2) + pow(acelY,2))) *(180.0/3.14);
 
       // show angles
-      Serial.print("Inclination in X: ");
+      Serial.print("Acelerometro X: ");
       Serial.print(aceel_anng_X);
-      Serial.print("  Inclination in Y: ");
-      Serial.println(aceel_anng_Y);
-
+      Serial.print("  Acelerometro Y: ");
+      Serial.print(aceel_anng_Y);
+      Serial.print("  Acelerometro Z: ");
+      Serial.println(aceel_anng_Z);
+    
       //Read angular speed
       mpu.getRotation(&gyroscopeX, &gyroscopeY, &gyroscopeZ );
 
+      dt = millis()-preview_time;
+      preview_time = millis();
+
       // Claculation angle of rotation
-      float girosc_ang_X= (gyroscopeX/1.31) * dt/1000.0 + last_gyroscopeX;
-      float girosc_ang_Y= (gyroscopeY/1.31) * dt/1000.0 + last_gyroscopeY;
+      float girosc_ang_X= (gyroscopeX/131)*(dt/1000.0) + last_gyroscopeX;
+      float girosc_ang_Y= (gyroscopeY/131)*(dt/1000.0) + last_gyroscopeY;
+      float girosc_ang_Z= (gyroscopeZ/131)*(dt/1000.0) + last_gyroscopeZ;
 
       last_gyroscopeX = girosc_ang_X;
       last_gyroscopeY = girosc_ang_Y;
+      last_gyroscopeZ = girosc_ang_Z;
       // show angles
-      Serial.print("Rotation in X: ");
+       Serial.print("Giroscopio X: ");
       Serial.print(gyroscopeX);
-      Serial.print("  Rotation in Y: ");
-      Serial.println(gyroscopeY);
+      Serial.print("  Giroscopio Y: ");
+      Serial.print(gyroscopeY);
+      Serial.print("  Giroscopio Z: ");
+      Serial.println(gyroscopeZ);
 
-      
 
 
   }
+
+
  
 }
